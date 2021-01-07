d1e326712addb6e07a6cc0feb819d956810d4612
Kuber aaganja <kuber@gurzu.com>
2021-01-07 22:13:10 +0545

    variable types in rust

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
-  let mut x = 45; // mut make the vairable mutable if not defined then we can not overwrite the value of x
+  let x = 45; 
+  /*
+  i32 is default value 32 bit integer
+  let x: i64 = 45; change to 64 bit integer
+  u64 is for positive 64 bit value
+  f32, f64 for the float
+  bool is the boolean variable
+  */
 
   println!("The value of x is {}", x);
   
